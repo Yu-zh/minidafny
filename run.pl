12ea9f995860947470e7d2a0fd1bb91223cceacf
bowen <bowen.huang@yale.edu>
2019-10-19 22:15:57 -0400

    done 1.0

--- run.pl
+++ run.pl
@@ -58,6 +58,7 @@ sub init_check()
             $BENCH_INFO{$name}{'path'}  = $file;
             $BENCH_INFO{$name}{'valid'} = $validness;
             
+            say "found benchmark $file";
             &info_print(3, "found benchmark");
             &info_print(5, "found benchmark file $file, type is $validness");
         }
